ec601169e7a4cdfc1cc4fe457c1994e8e6917e6c
Teehxw <112659668+Teehxw@users.noreply.github.com>
2023-11-02 13:30:27 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -38,6 +38,20 @@ def LinearSearch(array, item):
     
     return -1
 
+def BinarySearch(array,item):
+  lowIndex = 0
+  upperIndex = len(array) - 1
+  while lowIndex <= upperIndex:
+     midIndex = (upperIndex + lowIndex) // 2
+     if item == array[midIndex]:
+        return midIndex
+     elif item < array[midIndex]:
+        upperIndex = midIndex -1
+     else:
+        lowIndex = midIndex + 1
+
+  return -1
+
 # Main Program Loop
 loop = True
 while loop:
@@ -61,30 +75,58 @@ while loop:
     print("Spell Check a Word(Linear Search)")
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     word = input("Please Enter a Word: ")
-    
-    print(f"{word} is IN the Dictionary at postition {LinearSearch(dictionary,word)}")
+    if LinearSearch(dictionary,word)== -1:
+      print(f"{word} is NOT in Dictionary")
+    else:
+      print(f"{word} is IN the Dictionary at postition {LinearSearch(dictionary,word)}")
 
 
   # Option 2
   elif option == "2":
     #ADD ITEM TO END
     print("Spell check a Word(Binary Search)")
+    dictionary = loadWordsFromFile("data-files/dictionary.txt")
+    word = input("Please Enter a Word: ")
+    
+    index = BinarySearch(dictionary, word)
+    
+    if index == -1:
+      print(f"{word} is NOT in Dictionary")
+    else:
+      print(f"{word} is IN the Dictionary at postition {BinarySearch(dictionary,word)}")
 
 
   # Option 3
   elif option == "3":
     # REMOVE LAST ITEM
     print(" Spell Check in Alice In Wonderland(Linear Search)")
- 
+    aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
+    word = input("Please Enter a Word: ")
+    if LinearSearch(aliceWords,word)== -1:
+      print(f"{word} is NOT in Dictionary")
+    else:
+      print(f"{word} is IN the Dictionary at postition {LinearSearch(aliceWords,word)}")
+
 
   # Option 4
   elif option == "4":
     #INSERT AT POSITION
     print("Spell Check in Alice in Wonerland(Binary Search)")
-   
+    aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
+    word = input("Please Enter a Word: ")
+    if BinarySearch(aliceWords,word)== -1:
+      print(f"{word} is NOT in Dictionary")
+    else:
+      print(f"{word} is IN the Dictionary at postition {BinarySearch(aliceWords,word)}")
 
   # Option 5
   elif option == "8":
     print("EXIT")
     loop = False
 
+# EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
+#startTime = time.time()
+#insertionSort(randomData)
+
+#endTime = time.time()
+#print(f"Insertion Sort Random Data: {endTime - startTime} seconds")
